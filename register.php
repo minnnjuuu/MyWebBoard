e8d71d25818304ecb28cfdcd92efb9df1042b76d
김민주 <kmj020411@naver.com>
2024-09-29 10:45:08 +0900

    Update register.php

--- register.php
+++ register.php
@@ -5,7 +5,7 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 session_start();
-$conn = new mysqli('127.0.0.1', 'minnnjuuu', '020411', 'board');
+$conn = new mysqli('127.0.0.1', 'secret', 'secret', 'board');
 
 // 연결 오류 처리
 if ($conn->connect_error) {
